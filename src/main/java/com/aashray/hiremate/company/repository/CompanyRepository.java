55d560858d36adb01a832ed9d7424e32b7bf48d0
aashray374 <aashraymahajan374@gmail.com>
2025-12-24 13:10:40 +0530

    Created Company Repository

--- src/main/java/com/aashray/hiremate/company/repository/CompanyRepository.java
+++ src/main/java/com/aashray/hiremate/company/repository/CompanyRepository.java
@@ -1,9 +1,17 @@
 package com.aashray.hiremate.company.repository;
 
 import com.aashray.hiremate.company.entity.Company;
+import com.aashray.hiremate.user.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CompanyRepository extends JpaRepository<Company,Long> {
+    Page<Company> findAllByUserAndLocationIgnoreCase(User user, String location, Pageable pageable);
+
+    Company findByUserAndNameIgnoreCase(User user, String name);
+
+    Page<Company> findAllByUser(User user, Pageable pageable);
 }
