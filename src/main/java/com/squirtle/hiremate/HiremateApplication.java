3c74914dcf278397ffc5595f31a764171f1c4f04
aashray374 <aashraymahajan374@gmail.com>
2026-01-28 17:44:53 +0530

    Initial commit

---
+++ src/main/java/com/squirtle/hiremate/HiremateApplication.java
@@ -0,0 +1,13 @@
+package com.squirtle.hiremate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiremateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiremateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/squirtle/hiremate/HiremateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.squirtle.hiremate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiremateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
